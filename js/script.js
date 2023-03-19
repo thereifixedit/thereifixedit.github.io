889afa2f2aaf25f995bf3eb90dca378bacf99e49
thereifixedit <128231045+thereifixedit@users.noreply.github.com>
2023-03-19 14:52:34 +0800

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-the-bennetts-online","displayName":"The Bennetts Online","link":{"linkType":"LinkTypePage","href":"#!page-the-bennetts-online"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-page-2","displayName":"Page 2","link":{"linkType":"LinkTypePage","href":"#!page-page-2"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-page-3","displayName":"Page 3","link":{"linkType":"LinkTypePage","href":"#!page-page-3"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-page-4","displayName":"Page 4","link":{"linkType":"LinkTypePage","href":"#!page-page-4"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-the-bennetts-online","displayName":"The Bennetts Online","link":{"linkType":"LinkTypePage","href":"#!page-the-bennetts-online"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
