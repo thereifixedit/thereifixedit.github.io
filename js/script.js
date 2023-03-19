38085184d16d9169f160253f1870cea2b73fe103
thereifixedit <128231045+thereifixedit@users.noreply.github.com>
2023-03-19 21:15:51 +0800

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-the-bennetts-online","displayName":"The Bennetts Online","link":{"linkType":"LinkTypePage","href":"#!page-the-bennetts-online"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-welcome","displayName":"Welcome","link":{"linkType":"LinkTypePage","href":"#!page-welcome"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
