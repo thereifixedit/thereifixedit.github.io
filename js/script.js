bff10f501c8b044fdffa4b28acd11c2df4088d59
thereifixedit <davidk.bennett@gmail.com>
2024-05-24 21:14:00 +0800

    NewVersion

---
+++ js/script.js
@@ -0,0 +1,4 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-welcome","displayName":"Welcome","link":{"linkType":"LinkTypePage","href":"#!page-welcome"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
